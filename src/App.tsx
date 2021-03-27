9fd8c7f2e066135b611ffcddbe682e2e0497d11e
Mukul Jain <jainmukul1996@gmail.com>
2021-03-27 17:22:07 +0530

    feat: add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "normalize.css";
+import { Provider } from "react-redux";
 
 import "theme/index.scss";
+import store from "store";
 import Desktop from "desktop";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Desktop} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Desktop} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
